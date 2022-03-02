3989100a7fb2c047dcd71b5ff24cea8130f080cd
Neil Sawhney <neil@neil-sawhney.com>
2022-03-02 04:54:40 -0500

    more stuff

--- main.ino
+++ main.ino
@@ -1,53 +1,33 @@
 /* pin layout
 buttons:
-blue1,blue2,blue3,blue4,blue5 = 0,1,2,3,4;
-green1,green2,green3,green4,green5 = 5,6,7,8,9;
+    blue buttons 1-5 = pins 0-4;
+    green buttons 1-5 = pins 5-9;
 
 leds:
-blue1,blue2,blue3,blue4,blue5 = 10,11,12,13,14;
-green1,green2,green3,green4,green5 = 15,16,17,18,19;
+    blue buttons 1-5 = pins 10-14;
+    green buttons 1-5 = pins 15-19;
 */
 
-//returns true if any button is being pressed
-bool startConditionMet();
+//button presses necessary to win the game
+#define score_WIN = 50;
 
-//accepts a button pin, and returns the corresponding led pin
-int correspondingLED(int pin);
 
-//returns a random integer from 0 to 4 inclusive and sets that pins corresponding led to high
-int selectRandomButton_BLUE();
-
-//returns a random integer from 5 to 9 inclusive and sets that pins corresponding led to high
-int selectRandomButton_GREEN();
-
-//accepts a button pin, and sets the corresponding led low
-void killLED(int pin);
-
-//turns off all the leds
-void killAllLEDs();
-
-//flash all the blue buttons, and turn off all the green ones.
-void flashWinner_BLUE();
-
-//flash all the green buttons, and turn off all the blue ones.
-void flashWinner_GREEN();
-
- 
 //keeps track of the currently lit up button
 int activeButton_BLUE;
 int activeButton_GREEN;
 
-//true when game is in progress
+//true when game is in progress (for the loading screen)
 bool gameInProgress = false;
-
-//button presses necessary to win the game
-int score_WIN = 50;
 
 //keeps track of button presses
 int score_BLUE = 0;
 int score_GREEN = 0;
 
-//initialize all button pins as inputs and all led pins as outputs
+/*
+initialize all button pins as inputs and all led pins as outputs
+also set all leds to LOW
+*/
+
 void setup(){
     
 
@@ -92,8 +72,28 @@ void loop(){
     
 }
 
-//function implementations 
+//returns true if any button is being pressed
+bool startConditionMet();
 
+//accepts a button pin, and returns the corresponding led pin
 int correspondingLED(int pin){
     return pin + 10;
 };
+
+//returns a random integer from 0 to 4 inclusive and sets that pins corresponding led to high
+int selectRandomButton_BLUE();
+
+//returns a random integer from 5 to 9 inclusive and sets that pins corresponding led to high
+int selectRandomButton_GREEN();
+
+//accepts a button pin, and sets the corresponding led low
+void killLED(int pin);
+
+//turns off all the leds
+void killAllLEDs();
+
+//flash all the blue buttons, and turn off all the green ones.
+void flashWinner_BLUE();
+
+//flash all the green buttons, and turn off all the blue ones.
+void flashWinner_GREEN();
